d2515efe7af82529f200b5b7997173a7411336d0
Fazly Rabby <kmfrabby@gmail.com>
2023-07-17 09:16:49 +0600

    add: navbar

---
+++ src/components/organism/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="bg-gray-800 p-4">
+    <div class="container mx-auto flex justify-between items-center">
+      <router-link to="/" class="text-white text-2xl font-bold">
+        E-Commerce
+      </router-link>
+      <div class="flex space-x-4">
+        <router-link to="/" class="text-white hover:text-gray-300"
+          >Home</router-link
+        >
+        <router-link to="/cart" class="text-white hover:text-gray-300">
+          Cart ({{ cartItemsCount }})
+        </router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { computed } from "vue";
+import { useCart } from "@/stores/cart";
+
+export default {
+  setup() {
+    const cartStore = useCart();
+
+    const cartItemsCount = computed(() => cartStore.cartItemsCount);
+
+    return {
+      cartItemsCount,
+    };
+  },
+};
+</script>
+
+<style></style>
